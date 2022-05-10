c4f7444954d8eaca61588c942a7b9cd6a5760cbb
José Bueno <jomibusa1903@gmail.com>
2022-05-10 11:31:03 -0500

    Se implementan diseños para mostar los item en el recycler, se inicia implementación de métodos para el consumo de Retrofit

---
+++ app/src/main/java/com/jomibusa/pruebaceiba/data/RetrofitManager.kt
@@ -0,0 +1,50 @@
+package com.jomibusa.pruebaceiba.data
+
+import android.util.Log
+import com.jomibusa.pruebaceiba.interfaces.ApiService
+import com.jomibusa.pruebaceiba.model.Post
+import com.jomibusa.pruebaceiba.model.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class RetrofitManager {
+
+    private val TAG = "RetrofitManager"
+
+    fun getListUsers(listener: (List<User>?) -> Unit) {
+        val apiService = ApiService.create().getUsers()
+        apiService.enqueue(object : Callback<List<User>> {
+            override fun onResponse(call: Call<List<User>>?, response: Response<List<User>>?) {
+                if (response?.body() != null) {
+                    listener(response.body())
+                    Log.d(TAG, "onResponse ${response.body()}")
+                }
+            }
+
+            override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                listener(null)
+                Log.d(TAG, "onFailure ${t.message}")
+            }
+        })
+    }
+
+    fun getListPosts(userID: String, listener: (List<Post>?) -> Unit) {
+        val apiService = ApiService.create().getPostsByUser(userID)
+        apiService.enqueue(object : Callback<List<Post>> {
+            override fun onResponse(call: Call<List<Post>>?, response: Response<List<Post>>?) {
+                if (response?.body() != null) {
+                    listener(response.body())
+                    Log.d(TAG, "onResponse ${response.body()}")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+                listener(null)
+                Log.d(TAG, "onFailure ${t.message}")
+            }
+        })
+    }
+
+}
--- app/src/main/java/com/jomibusa/pruebaceiba/interfaces/ApiService.kt
+++ app/src/main/java/com/jomibusa/pruebaceiba/interfaces/ApiService.kt
@@ -4,6 +4,8 @@ import com.jomibusa.pruebaceiba.model.Post
 import com.jomibusa.pruebaceiba.model.User
 import com.jomibusa.pruebaceiba.util.EndPointList
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,9 +13,22 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET(EndPointList.GET_USERS)
-    fun getUsers(): Call<User>
+    fun getUsers(): Call<List<User>>
 
     @GET(EndPointList.GET_POST_USER)
-    fun getPostsByUser(@Query("userId") userId: String): Call<Post>
+    fun getPostsByUser(@Query("userId") userId: String): Call<List<Post>>
+
+    companion object {
+
+        var BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+        fun create(): ApiService {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+    }
 
 }
